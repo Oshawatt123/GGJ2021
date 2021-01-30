1b5794762d19e794feee0d671ab3532ecdc1aef6
Oshawatt123 <lewis.arnold@hotmail.co.uk>
2021-01-30 12:18:41 +0000

    WeatherManager started

--- MessageInABottle/Assets/Scripts/BottleEvent.cs
+++ MessageInABottle/Assets/Scripts/BottleEvent.cs
@@ -27,4 +27,9 @@ public class BottleEvent : MonoBehaviour
     {
         throw new NotImplementedException();
     }
+
+    public void StartEvent()
+    {
+        transform.position = EventManager.Instance().EventSpawnPoint.position;
+    }
 }
