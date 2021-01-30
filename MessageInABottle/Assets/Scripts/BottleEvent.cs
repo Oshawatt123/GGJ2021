67ba2c3315620820bf2bfb55835d4632eaf56065
Oshawatt123 <lewis.arnold@hotmail.co.uk>
2021-01-30 12:00:30 +0000

    Ligtning asset added

---
+++ MessageInABottle/Assets/Scripts/BottleEvent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BottleEvent : MonoBehaviour
+{
+    public UnityEvent startEvents;
+
+    public UnityEvent passEvent;
+    public UnityEvent failEvent;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        throw new NotImplementedException();
+    }
+}
