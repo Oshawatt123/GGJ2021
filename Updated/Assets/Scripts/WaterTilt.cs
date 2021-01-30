06a2e509ca3b66723ce49fd26ffcc37b34885d2e
Marques Scripps <marques_scripps@outlook.com>
2021-01-30 15:36:34 +0000

    Update

---
+++ Updated/Assets/Scripts/WaterTilt.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterTilt : MonoBehaviour
+{
+    [SerializeField] private Transform origin;
+    [SerializeField] private LayerMask waterLayer;
+
+    [SerializeField] private float followWaterDistance;
+
+    [SerializeField] private MeshFilter meshFilter;
+
+    private bool hittingWater;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(origin.position, -transform.up, out hit, followWaterDistance, waterLayer))
+        {
+            transform.up = hit.normal;
+            hittingWater = true;
+            Debug.Log(hit.transform.name);
+            
+            // get normal from face of mesh
+            Debug.DrawRay(hit.transform.position, hit.normal, Color.cyan);
+        }
+        else hittingWater = false;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (hittingWater) Gizmos.color = Color.green;
+        else Gizmos.color = Color.magenta;
+        
+        Gizmos.DrawRay(origin.position, -transform.up * followWaterDistance);
+    } 
+}
