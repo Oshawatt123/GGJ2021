4bf069360c4700ad1b8a1a17b64c0935130f68d2
Oshawatt123 <lewis.arnold@hotmail.co.uk>
2021-01-31 13:20:22 +0000

    Narrative added & event animations
    Boat & seagull events animated

---
+++ MessageInABottle/Assets/Scripts/StormEvent.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Timeline;
+using UnityEngine;
+
+public class StormEvent : GameEvent
+{
+    [SerializeField] private CanvasGroup cg;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Reset());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void SpawnEvent(BottleEvent eventTrigger)
+    {
+        Debug.Log(eventTrigger.gameObject.name);
+        base.SpawnEvent(eventTrigger);
+        Setup();
+    }
+
+    private void Setup()
+    {
+        cg.alpha = Mathf.Lerp(0, 1, 1f);
+        cg.blocksRaycasts = true;
+        cg.interactable = true;
+    }
+
+    private IEnumerator Reset(float wait = 0f)
+    {
+        yield return new WaitForSeconds(wait);
+        cg.alpha = Mathf.Lerp(1, 0, 1f);
+        cg.blocksRaycasts = false;
+        cg.interactable = false;
+        transform.position = new Vector3(0, -10, 0);
+    }
+
+    public void Pass()
+    {
+        trigger.Pass();
+        StartCoroutine(Reset());
+    }
+
+    public void Fail()
+    {
+        Debug.Log(trigger.gameObject.name);
+        trigger.Fail();
+        
+        StartCoroutine(Reset());
+    }
+}
---
+++ MessageInABottle/Assets/Scripts/WindEvent.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Timeline;
+using UnityEngine;
+
+public class WindEvent : GameEvent
+{
+    [SerializeField] private CanvasGroup cg;
+
+    private Animator anim;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Reset());
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void SpawnEvent(BottleEvent eventTrigger)
+    {
+        if (anim) anim.SetTrigger("Float");
+        Debug.Log(eventTrigger.gameObject.name);
+        base.SpawnEvent(eventTrigger);
+        transform.position = new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z);
+        Setup();
+    }
+
+    private void Setup()
+    {
+        cg.alpha = Mathf.Lerp(0, 1, 1f);
+        cg.blocksRaycasts = true;
+        cg.interactable = true;
+    }
+
+    private IEnumerator Reset(float wait = 0f)
+    {
+        yield return new WaitForSeconds(wait);
+        cg.alpha = Mathf.Lerp(1, 0, 1f);
+        cg.blocksRaycasts = false;
+        cg.interactable = false;
+        transform.position = new Vector3(0, -10, 0);
+    }
+
+    public void Pass()
+    {
+        if (anim) anim.SetTrigger("Sink");
+        trigger.Pass();
+        StartCoroutine(Reset(1f));
+    }
+
+    public void Fail()
+    {
+        if (anim) anim.SetTrigger("BlowAway");
+        Debug.Log(trigger.gameObject.name);
+        trigger.Fail();
+        
+        StartCoroutine(Reset(2.4f));
+    }
+}
